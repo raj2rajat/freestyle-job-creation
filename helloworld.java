200feb01995c24c51a8a3490a242e98bd42d2199
Ubuntu <ubuntu@ip-172-31-88-47.ec2.internal>
2022-07-09 00:23:23 +0000

    modifying helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-	System.out.println("Hello, World, I am back");
+	System.out.println("Hello, World, I am back to rock and roll");
   }
 }
